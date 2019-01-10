ceba5fd09eb6bd31d8782dab02b1005ba666ae25
Yang Qiwen <yangqiwen@pachiratech.com>
2019-01-10 12:38:54 +0800

    add java8 package

---
+++ Test_idea/src/main/java/com/test/java8/TestClosure.java
@@ -0,0 +1,41 @@
+package com.test.java8;
+
+import org.junit.Test;
+
+public class TestClosure {
+
+    public static Supplier<Integer> testClosure() {
+        final int i = 1;
+        return new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return i;
+            }
+        };
+    }
+
+    public static Supplier<Integer> testClosure2() {
+        int i = 1;
+        return () -> {
+            return i;
+        };
+    }
+
+    @FunctionalInterface
+    public interface Supplier<T> {
+        T get();
+
+        default T set() {
+            return null;
+        }
+
+
+    }
+
+    @Test
+    public void t() {
+        System.out.println("start");
+        System.out.println(testClosure().get());
+        System.out.println(testClosure2().get());
+    }
+}
