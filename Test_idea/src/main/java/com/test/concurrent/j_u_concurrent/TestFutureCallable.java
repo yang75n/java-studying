6eb4245e2317930531d92f7b692f91f0a434a3a6
Yang Qiwen <yangqiwen@pachiratech.com>
2019-05-05 12:59:07 +0800

    add forkjoinpool

--- Test_idea/src/main/java/com/test/concurrent/j_u_concurrent/TestFutureCallable.java
+++ Test_idea/src/main/java/com/test/concurrent/j_u_concurrent/TestFutureCallable.java
@@ -3,7 +3,7 @@ package com.test.concurrent.j_u_concurrent;
 
 import java.util.concurrent.*;
 
-public class TestCallable {
+public class TestFutureCallable {
 
     static class SumTask implements Callable<Long> {
 
@@ -20,13 +20,15 @@ public class TestCallable {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.out.println("Start:" + System.currentTimeMillis());
         long nanotime = System.nanoTime();
-        FutureTask<Long> futureTask = new FutureTask<Long>(new SumTask());
-        Executor executor = Executors.newSingleThreadExecutor();
-        executor.execute(futureTask);
-        //new Thread(futureTask).start();
-        System.out.println(futureTask.get());
+
+        FutureTask<Long> futureTask = new FutureTask<>(new SumTask());
+//        Executor executor = Executors.newSingleThreadExecutor();
+//        executor.execute(futureTask);
+        new Thread(futureTask).start();
+        System.out.println("future get="+futureTask.get());
+
         System.out.println("End:" + System.currentTimeMillis());
-        System.out.println(System.nanoTime() - nanotime);
+        System.out.println("use time:" + (System.nanoTime() - nanotime));
     }
 
 }
